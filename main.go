9e9570842de2b1d10f7435f8e77acf2ddd523f71
Павел Сашков <paul.sashkov@gmail.com>
2025-01-30 15:26:48 +0300

    add .env

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	webDir := "./web"
+	r := chi.NewRouter()
+	fileServer := http.FileServer(http.Dir(webDir))
+	r.Mount("/", fileServer)
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7550"
+	}
+	if err := http.ListenAndServe(port, r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
